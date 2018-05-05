020b49351dc6b519c935af8c746855b4efe68fbb
Sourabh <mangal.sourabh@gmail.com>
2018-05-05 23:19:43 +0530

    Added Map relocate function

--- public/js/main.js
+++ public/js/main.js
@@ -46,6 +46,24 @@ $(document).ready(function(){
        $(".card2").fadeOut(300);
        $(currentId).delay(300).fadeIn();
    });
+
+   $('.location1 a').on('click', function(event){
+       event.preventDefault();
+       $('.location2 a').removeClass('active');  
+       $(this).addClass("active");
+       // var currentId = $(this).attr('href');
+       // $(".card2").fadeOut(300);
+       // $(currentId).delay(300).fadeIn();
+   });
+
+   $('.location2 a').on('click', function(event){
+       event.preventDefault();
+       $('.location1 a').removeClass('active');  
+       $(this).addClass("active");
+       // var currentId = $(this).attr('href');
+       // $(".card2").fadeOut(300);
+       // $(currentId).delay(300).fadeIn();
+   });
     
     //show arrows
     $('.evnt-calendar').on('mouseenter', function(){
@@ -94,37 +112,211 @@ $(document).ready(function(){
 });
 
 // GOOGLE MAP SETTING from https://snazzymaps.com/style/151/ultra-light-with-labels
-
 // When the window has finished loading create our google map below
-            google.maps.event.addDomListener(window, 'load', init);
-        
-            function init() {
-                // Basic options for a simple Google Map
-                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
-                    zoom: 15,
+// We are using a div with id="map" seen below in the <body>
+var mapElement = document.getElementById('map');
+var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 15,
+        // How you would like to style the map. 
+        // This is where you would paste any style found on Snazzy Maps.
+        styles:[  
+         {  
+            "featureType":"water",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#e9e9e9"
+               },
+               {  
+                  "lightness":17
+               }
+            ]
+         },
+         {  
+            "featureType":"landscape",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#f5f5f5"
+               },
+               {  
+                  "lightness":20
+               }
+            ]
+         },
+         {  
+            "featureType":"road.highway",
+            "elementType":"geometry.fill",
+            "stylers":[  
+               {  
+                  "color":"#ffffff"
+               },
+               {  
+                  "lightness":17
+               }
+            ]
+         },
+         {  
+            "featureType":"road.highway",
+            "elementType":"geometry.stroke",
+            "stylers":[  
+               {  
+                  "color":"#ffffff"
+               },
+               {  
+                  "lightness":29
+               },
+               {  
+                  "weight":0.2
+               }
+            ]
+         },
+         {  
+            "featureType":"road.arterial",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#ffffff"
+               },
+               {  
+                  "lightness":18
+               }
+            ]
+         },
+         {  
+            "featureType":"road.local",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#ffffff"
+               },
+               {  
+                  "lightness":16
+               }
+            ]
+         },
+         {  
+            "featureType":"poi",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#f5f5f5"
+               },
+               {  
+                  "lightness":21
+               }
+            ]
+         },
+         {  
+            "featureType":"poi.park",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#dedede"
+               },
+               {  
+                  "lightness":21
+               }
+            ]
+         },
+         {  
+            "elementType":"labels.text.stroke",
+            "stylers":[  
+               {  
+                  "visibility":"on"
+               },
+               {  
+                  "color":"#ffffff"
+               },
+               {  
+                  "lightness":16
+               }
+            ]
+         },
+         {  
+            "elementType":"labels.text.fill",
+            "stylers":[  
+               {  
+                  "saturation":36
+               },
+               {  
+                  "color":"#333333"
+               },
+               {  
+                  "lightness":40
+               }
+            ]
+         },
+         {  
+            "elementType":"labels.icon",
+            "stylers":[  
+               {  
+                  "visibility":"off"
+               }
+            ]
+         },
+         {  
+            "featureType":"transit",
+            "elementType":"geometry",
+            "stylers":[  
+               {  
+                  "color":"#f2f2f2"
+               },
+               {  
+                  "lightness":19
+               }
+            ]
+         },
+         {  
+            "featureType":"administrative",
+            "elementType":"geometry.fill",
+            "stylers":[  
+               {  
+                  "color":"#fefefe"
+               },
+               {  
+                  "lightness":20
+               }
+            ]
+         },
+         {  
+            "featureType":"administrative",
+            "elementType":"geometry.stroke",
+            "stylers":[  
+               {  
+                  "color":"#fefefe"
+               },
+               {  
+                  "lightness":17
+               },
+               {  
+                  "weight":1.2
+               }
+            ]
+         }
+      ]
+    };
 
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(26.8942357, 75.8229582), // Raja Park
+google.maps.event.addDomListener(window, 'load', locate_rajapark);
 
-                    // How you would like to style the map. 
-                    // This is where you would paste any style found on Snazzy Maps.
-                    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-                };
+function locate_mansarover() {
+  // The latitude and longitude to center the map (always required)
+  mapOptions.center = new google.maps.LatLng(26.8435679,75.7686004)
+  var map = new google.maps.Map(mapElement, mapOptions);
+  var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(26.8435679,75.7686004),
+      map: map,
+  });
+}
 
-                // Get the HTML DOM element that will contain your map 
-                // We are using a div with id="map" seen below in the <body>
-                var mapElement = document.getElementById('map');
-
-                // Create the Google Map using our element and options defined above
-                var map = new google.maps.Map(mapElement, mapOptions);
-
-                // Let's also add a marker while we're at it
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(26.8942357, 75.8229582),
-                    map: map,
-                    title: 'Snazzy!'
-                });
-            }
+function locate_rajapark() {
+  // The latitude and longitude to center the map (always required)
+  mapOptions.center = new google.maps.LatLng(26.894253,75.821861)
+  var map = new google.maps.Map(mapElement, mapOptions);
+  var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(26.894253,75.821861),
+      map: map,
+  });
+}
     
